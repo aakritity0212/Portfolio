349a37e8a28f9da8ec5ddb6f8900b33fbb6b7dd8
aakritity0212 <aaty9260@gmail.com>
2025-09-03 17:47:24 +0530

    Experience Section done

--- Portfolio/src/constants.js
+++ Portfolio/src/constants.js
@@ -1,3 +1,4 @@
+// Skills section Logo
 import htmlLogo from './assets/html-logo.png';
 import cssLogo from './assets/css-logo.png';
 import reactjsLogo from './assets/reactjs-logo.png';
@@ -13,7 +14,10 @@ import githubLogo from './assets/github.png';
 import vscodeLogo from './assets/vscode.png';
 import postmanLogo from './assets/postman.png';
 
+// Experience Section Logo
+import enpassLogo from './assets/enpass_logo.png'
 
+// Skills Section
 export const SkillsInfo = [
   {
     title: 'Frontend',
@@ -46,3 +50,37 @@ export const SkillsInfo = [
     ],
   },
 ];
+
+// Experience Section
+export const experiences = [
+    {
+      id: 0,
+      img: enpassLogo,
+      role: "Software Engineer I",
+      company: "Enpass Technologies",
+      date: "September 2024 - Present",
+      desc: "Worked on the Dashboard Page for the Enpass Admin Console using React, providing users with a comprehensive view of Business plan license summaries, total users, and provisioning status. Contributed on API Automation framework to automate the APIs used in Enpass Admin Console using Python and Postman.",
+      skills: [
+        "API Automation",
+        "Python",
+        "Postman",
+        "React JS",
+        "Web Development"
+      ],
+    },
+    {
+      id: 1,
+      img: enpassLogo,
+      role: "Software Engineer Trainee",
+      company: "Enpass Technologies",
+      date: "September 2023 - August 2024",
+      desc: "Conducted manual testing of web and mobile applications, ensuring a smooth user experience through thorough test case execution. Collaborated with development teams to identify and resolve defects, contributing to the successful releases of the product.",
+      skills: [
+        "Agile methodologies",
+        "SDLC",
+        "JIRA",
+        "Desktop Automation",
+        "JAVA"
+      ],
+    },
+  ];
